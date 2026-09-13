db7aaa7000a4ab74c3c630340db87764e01ffcaa
CodeWithCJ <151883488+CodeWithCJ@users.noreply.github.com>
2026-09-13 19:30:21 +0000

    chore(nix): update pnpm deps hashes

--- nix/frontend.nix
+++ nix/frontend.nix
@@ -30,7 +30,7 @@ stdenv.mkDerivation (finalAttrs: {
       pnpmWorkspaces
       ;
     fetcherVersion = 3;
-    hash = "sha256-m15OgA3Th9OJBRqM21mN/RrTvGEgKM//0bfb1muHHcw=";
+    hash = "sha256-G1mSUrL5q2kZzTxtRde9fp0G4lIC/ib3R8TEI/v4f/g=";
   };
 
   nativeBuildInputs = [
